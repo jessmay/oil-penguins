5179b6a968e10c5198cd2d6838fb8a4e5c75f417
Jlinge <Joshua@Joshua-Linges-MacBook-Pro.local>
2014-02-05 17:21:40 -0500

    Added map class for uniform wall placement
    Clicking will add a wall to the location or remove an already placed
    wall.
    
    Todo:
    Make sure agents cannot be placed intersecting a wall or another agent.
    Be able to remove agents by clicking on them.

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -6,53 +6,38 @@ public class Background : MonoBehaviour {
 	public GameObject Wall;
 	public GameObject Agent;
 
-	private int width = 20;
-	private int height = 20;
+	private int mapWidth = 20;
+	private int mapHeight = 20;
 
 	private int gridWidth = 10;
 	private int gridHeight = 10;
 
 
 	public Grid grid;
+	public Map map;
 
 	// Use this for initialization
 	void Start () {
 
 		grid = new Grid (gridWidth, gridHeight, renderer.bounds);
-
-		float xSize = renderer.bounds.size.x/width;
-		float ySize = renderer.bounds.size.y/height;
-
-		float wallX = xSize/(Wall.renderer.bounds.size.x/Wall.transform.localScale.x);
-		float wallY = ySize/(Wall.renderer.bounds.size.y/Wall.transform.localScale.y);
-
-		Wall.transform.localScale = new Vector3(wallX, wallY, 1);
-
-		Vector3 c = renderer.bounds.center;
-
-		for (int i = -height/2; i <= height/2; ++i) {
-			Instantiate (Wall, new Vector3(c.x+i*xSize, c.y-width/2*ySize, c.z), Wall.transform.rotation);
-			
-			Instantiate (Wall, new Vector3(c.x+i*xSize, c.y+width/2*ySize, c.z), Wall.transform.rotation);
-		}
-
-		for (int j = -width/2 +1; j < width/2; ++j) {
-			Instantiate (Wall, new Vector3(c.x-height/2*xSize, c.y+j*ySize, c.z), Wall.transform.rotation);
-
-			Instantiate (Wall, new Vector3(c.x+height/2*xSize, c.y+j*ySize, c.z), Wall.transform.rotation);
-		}
-
+		map = new Map(Wall,mapWidth, mapHeight, renderer.bounds);
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
+		//Place/remove wall at the given mouse location
 		if (Input.GetMouseButtonDown (0)) {
 			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			pos.z = 0;
-			Instantiate(Wall, pos, Wall.transform.rotation);
+
+			//If wall already exists at the location, remove it
+			if(!map.addWall(map.getCellIndex(pos)))
+				map.removeWall(map.getCellIndex(pos));
 		}
+
+		//Place agent at the given mouse location
 		if (Input.GetMouseButtonDown (1)) {
 			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			pos.z = 0;
